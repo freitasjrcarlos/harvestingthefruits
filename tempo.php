b05d60af9ab13c40cd17666bcb93efd14951f9ed
Carlos Freitas <carlos.freitas@turtleagencia.com.br>
2019-03-11 22:35:15 -0300

    Alterações na página win

--- tempo.php
+++ tempo.php
@@ -2,7 +2,7 @@
 
  <div class="container">
  	<div class="row center">
- 		<h1 class="titulo"><div class="animated infinite flash">Acabou o tempo!</div></h1>
+ 		<h1 class="titulo-robo"><div class="animated infinite flash">Acabou o tempo!</div></h1>
  		<img src="img/svg/relogio.svg" height="400px">
  	</div>
  </div>
--- youWin.php
+++ youWin.php
@@ -2,11 +2,15 @@
 <audio autoplay>
   <source src="snd/countryboy.mp3" type="audio/mp3" />
 </audio>
-<div class="container">
-	<div class="row center">
-		<h1 class="titulo-parabens"><div class="animate infinite lightSpeedIn">Parabéns!</div></h1>
+
+<div class="container-game">
+	<h1 class="titulo-parabens">Parabéns</h1>
+	</div>
+
+	<div class="container-game">
 		<a href="one.php"><button class="btn btn-game">Fase 1</button></a> <a href="two.php"><button class="btn btn-game">Fase 2</button></a>
 	</div>
-</div>
+
+
 
 <?php include 'footer.php' ?>
