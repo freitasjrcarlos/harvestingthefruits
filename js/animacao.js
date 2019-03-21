ac7ada9f23639f6a579354587fa91f679be2b23f
Carlos Freitas <carlos.freitas@turtleagencia.com.br>
2019-03-21 17:46:18 -0300

    Alterações arquivo animacao.js

--- js/animacao.js
+++ js/animacao.js
@@ -25,20 +25,20 @@ Animacao.prototype = {
       // Posso continuar?
       if ( ! this.ligado ) return;
       
-      var agora = new Date().getTime();
+      let agora = new Date().getTime();
       if (this.ultimoCiclo == 0) this.ultimoCiclo = agora;
       this.decorrido = agora - this.ultimoCiclo;
 
       // Atualizamos o estado dos sprites
-      for (var i in this.sprites)
+      for (let i in this.sprites)
          this.sprites[i].atualizar();
 
       // Desenhamos os sprites
-      for (var i in this.sprites)
+      for (let i in this.sprites)
          this.sprites[i].desenhar();
          
       // Processamentos gerais
-      for (var i in this.processamentos)
+      for (let i in this.processamentos)
          this.processamentos[i].processar();
          
       // Processamento de exclusões
@@ -48,7 +48,7 @@ Animacao.prototype = {
       this.ultimoCiclo = agora;
 
       // Chamamos o próximo ciclo
-      var animacao = this;
+      let animacao = this;
       requestAnimationFrame(function() {
          animacao.proximoFrame();
       });
@@ -65,16 +65,16 @@ Animacao.prototype = {
    },
    processarExclusoes: function() {
       // Criar novos arrays
-      var novoSprites = [];
-      var novoProcessamentos = [];
+      let novoSprites = [];
+      let novoProcessamentos = [];
       
       // Adicionar somente se não constar no array de excluídos
-      for (var i in this.sprites) {
+      for (let i in this.sprites) {
          if (this.spritesExcluir.indexOf(this.sprites[i]) == -1)
             novoSprites.push(this.sprites[i]);
       }
       
-      for (var i in this.processamentos) {
+      for (let i in this.processamentos) {
          if (this.processamentosExcluir.indexOf(this.processamentos[i])
              == -1)
             novoProcessamentos.push(this.processamentos[i]);
