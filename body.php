9ec5ac54d6dc38c97b28f1be00fb936dfc3cb15e
Carlos Freitas <carlos.freitas@turtleagencia.com.br>
2019-11-23 20:43:07 -0300

    Fixed 1900> layout

--- body.php
+++ body.php
@@ -6,14 +6,14 @@
   <div class="mensagem"> <h1> Game ainda não disponível para mobile :( </h1> </div>
   <!-- / -->
 
-<div class="alerta">
+<div class="animacao-home">
   <h1 class="titulo"> Harvesting the fruits</h1>
     <div class="container">
       <div class="row">
         <div class="animated rollIn">
           <img src="img/svg/sol.svg" height="250px">
         </div>
-           <div class="container-game">
+           <div class="container-game animacao">
               <img src="img/svg/fazenda.svg" height="250px"> 
             </div>     
                   
